333c5be1efe413350b40aadc454c1bdfd5e96d34
Michael Mittermayr <mmichael@edumail.at>
2017-05-17 13:33:56 +0200

    add platform-swagger

---
+++ platform-swagger/src/main/java/at/sintrum/fog/swagger/SwaggerConfig.java
@@ -0,0 +1,30 @@
+package at.sintrum.fog.swagger;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+/**
+ * Created by Michael Mittermayr on 17.05.2017.
+ */
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+
+        Logger logger = LoggerFactory.getLogger(SwaggerConfig.class);
+        logger.info("Init Swagger");
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build();
+    }
+}
