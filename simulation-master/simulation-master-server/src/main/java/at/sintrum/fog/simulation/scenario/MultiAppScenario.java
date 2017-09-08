adec3e011ed84f1f2dac8d7e0581d8dfbbf3dc29
Michael Mittermayr <mmichael@edumail.at>
2017-09-08 17:17:44 +0200

    multi app scenario

---
+++ simulation-master/simulation-master-server/src/main/java/at/sintrum/fog/simulation/scenario/MultiAppScenario.java
@@ -0,0 +1,63 @@
+package at.sintrum.fog.simulation.scenario;
+
+import at.sintrum.fog.simulation.scenario.dto.BasicScenarioInfo;
+import at.sintrum.fog.simulation.taskengine.TaskListBuilder;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by Michael Mittermayr on 08.09.2017.
+ */
+@Service
+public class MultiAppScenario implements Scenario {
+
+    private final TaskListBuilder taskListBuilder;
+
+    public MultiAppScenario(TaskListBuilder taskListBuilder) {
+        this.taskListBuilder = taskListBuilder;
+    }
+
+    @Override
+    public String getId() {
+        return "multiApp";
+    }
+
+    @Override
+    public TaskListBuilder.TaskListBuilderState build(BasicScenarioInfo basicScenarioInfo) {
+
+        TaskListBuilder.TaskListBuilderState taskListBuilderState = taskListBuilder.newTaskList(this);
+
+        taskListBuilderState.resetMetadata();
+
+        taskListBuilderState.createTrack()
+                .startTestApp(0, basicScenarioInfo.getCloud())
+                .requestApp(0, basicScenarioInfo.getFogA())
+                .requestApp(0, basicScenarioInfo.getFogB())
+                .requestApp(0, basicScenarioInfo.getFogC())
+                .checkLocation(0, basicScenarioInfo.getFogA())
+                .finishWork(0)
+                .checkLocation(0, basicScenarioInfo.getFogB())
+                .finishWork(0)
+                .checkLocation(0, basicScenarioInfo.getFogC())
+                .finishWork(0)
+                .checkLocation(0, basicScenarioInfo.getCloud())
+                .logMessage(0, "Track A finished")
+                .removeApp(0);
+
+        taskListBuilderState.createTrack()
+                .startAnotherApp(0, basicScenarioInfo.getCloud())
+                .requestApp(0, basicScenarioInfo.getFogA())
+                .requestApp(0, basicScenarioInfo.getFogB())
+                .requestApp(0, basicScenarioInfo.getFogC())
+                .checkLocation(0, basicScenarioInfo.getFogA())
+                .finishWork(0)
+                .checkLocation(0, basicScenarioInfo.getFogB())
+                .finishWork(0)
+                .checkLocation(0, basicScenarioInfo.getFogC())
+                .finishWork(0)
+                .checkLocation(0, basicScenarioInfo.getCloud())
+                .logMessage(0, "Track B finished")
+                .removeApp(0);
+
+        return taskListBuilderState;
+    }
+}
